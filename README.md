61963ab703d2e96c02305271be6e4d7c4843ba1d
Leo <184458875+leocoding0326@users.noreply.github.com>
2025-04-27 17:22:32 -0500

    Added pseudocode for user choice created a function to prompt the user with a number choice created a if conditional to convert the number to the right string

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Computer Choice
 Create a function called getComputerChoice and declare a variable called "num"
 Use Math.floor and Math.random * 3 to generate a number between 0 and 2
 Use If conditional to make console log paper, scissors and rock in different numbers
+
+User Choice
+User will input their choice thru a prompt
+Create a function called getHumanChoice
+Declare a variable called "choice"
+Will use prompt to give the choice to the user between numbers 1(rock), 2 (paper), 3(scissors)
+Use if conditional to make each number match their string and log in the console that way.
--- java.js
+++ java.js
@@ -1,16 +1,41 @@
 
+/*computer choice*/
 function getComputerChoice (){
     
     const num = Math.floor (Math.random() * 3);
 
     if (num === 0){
-        return "rock";
+        return "pc choose rock";
     }
     else if (num === 1){
-         return "paper";
+         return "pc choose paper";
     }
     else{
-        return "scissors"
+        return "pc choose scissors"
     }
     }
 console.log(getComputerChoice())
+
+/* User Choice */
+
+function getHumanChoice (){
+    
+    let choice =prompt ("Enter 1 for rock, 2 for paper, 3 for scissors");
+    
+    choice=Number(choice)
+
+    if(choice === 1) {
+        console.log("rock")
+    }
+    else if (choice === 2) {
+        console.log("paper")
+    }
+    else if (choice === 3) {
+        console.log ("scissors")
+    }
+    else {
+        "invalid choice"
+    }
+}
+console.log(getHumanChoice())
+
